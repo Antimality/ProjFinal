29ca7b0d1b4e299e7d02a22820cac48c8275adbc
Jonatan Nitzan <yognatan33@gmail.com>
2025-09-17 13:52:34 +0300

    Deleted irrelevant code and set up makefile

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
 from setuptools import Extension, setup
 
-module = Extension("mykmeanspp", sources=["kmeansmodule.c", "kmeans.c"])
+module = Extension("mysymnmf", sources=["symnmfmodule.c", "symnmf.c"])
 setup(
-    name="mykmeanspp",
+    name="mysymnmf",
     version="1.0",
-    description="Python wrapper for kmeans C extension",
+    description="Python wrapper for symNMF extension",
     ext_modules=[module],
 )
