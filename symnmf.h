00d8b0c920449a3a6a42abe8b14f462d4b374cb8
yanai14 <yannair@mail.tau.ac.il>
2025-09-20 20:55:50 +0300

    Update symnmf.h
    symnmf h imp - before python connection

--- symnmf.h
+++ symnmf.h
@@ -1,6 +1,9 @@
 #ifndef SYMNMF_H
 #define SYMNMF_H
-
+#define MAX_ITER 300
+#define epsilon 0.0001
+#define beta 0.5
+#define delta 0.000001
 /*
  * ============================================================================
  * Function Prototypes
@@ -69,4 +72,80 @@ void free_matrix(double **matrix, int rows);
  */
 void handle_error();
 
+/**
+ * @brief calculates Euclidean distance of two vectors.
+ * @param vec1 The first vector.
+ * @param vec2 The second vector.
+ * @param dim The vectors size.
+ * @return Euclidean distance.
+ */
+double euclidean_distance(double *vec1, double *vec2, int dim);
+
+/**
+ * @brief calculates Frobenius norm of a 2D matrix.
+ * @param matrix The matrix to calculate.
+ * @param rows The number of rows in the matrix.
+ * @param cols The number of columns in the matrix.
+ * @param dim the vector dimansion.
+ * @return Frobenius norm.
+ */
+double frobenius_norm(double **matrix, int rows, int cols);
+
+/**
+ * @brief Returns the multiplication of two matrixes.
+ * @param matrix1 The  first matrix.
+ * @param matrix2 The  second matrix.
+ * @param row1 The first matrix number of rows.
+ * @param row2 The second matrix number of columns.
+ * @param cols1 The first matrix number of columns.
+ * @param cols2 The second matrix number of columns.
+ * @returns a pointer to the caluclated matrix.
+ */
+double **matrix_multiply(double **matrix1, double **matrix2, int row1, int row2, int cols1, int cols2 );
+
+/**
+ * @brief Returns the special multiplication of the H matrix.
+ * @param matrix The matrix to calculate.
+ * @param rows The number of rows in the matrix.
+ * @param cols The number of columns in the matrix.
+ * @returns pointer to a new H(H^T) matrix.
+ */
+double **H_multiply(double **matrix, int rows, int cols);
+
+/**
+ * @brief Returns the subtraction of two matrixes.
+ * @param matrix1 The first matrix.
+ * @param matrix2 The second matrix.
+ * @param rows The matrix number of rows.
+ * @param cols The first  number of columns.
+ * @returns a pointer to the caluclated matrix.
+ */
+double **matrix_subtract(double **matrix1, double **matrix2, int rows, int cols );
+
+/**
+ * @brief Returns the next iteration of H
+ * @param matrix_W The W matrix.
+ * @param matrix_H The H matrix.
+ * @param row_W The W matrix number of rows.
+ * @param row_H The H matrix number of columns.
+ * @param cols_W The W matrix number of columns.
+ * @param cols_H The H matrix number of columns.
+ * @returns a pointer to the caluclated matrix.
+ */
+double **H_update(double **matrix_W, double **matrix_H, int row_W, int row_H, int cols_W, int cols_H);
+
+/**
+ * @brief initilize a matrix full of 0.0
+ * @param rows The number of rows in the matrix.
+ * @param cols The number of columns in the matrix.
+ * @returns a pointer to the matrix/
+ */
+double **matrix_init(int rows, int cols);
+
+/**
+ * @brief changes the degree matrix D into D^(-0.5).
+ * @param D pointer to matrix D.
+ * @param n the size of the matrix.
+ */
+void inv_root(double **matrix, int n);
 #endif
