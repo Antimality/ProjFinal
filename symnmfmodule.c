25762865038c8f24defc3f807fa5a993dac91862
Jonatan Nitzan <yognatan33@gmail.com>
2025-09-25 18:26:59 +0300

    Added documentation and improved readability

--- symnmfmodule.c
+++ symnmfmodule.c
@@ -2,7 +2,7 @@
 #include "symnmf.h"
 #include <Python.h>
 
-// Forward declarations for the wrapper functions
+/* Forward declarations for the wrapper functions */
 static PyObject *sym_wrapper(PyObject *self, PyObject *args);
 static PyObject *ddg_wrapper(PyObject *self, PyObject *args);
 static PyObject *norm_wrapper(PyObject *self, PyObject *args);
@@ -14,7 +14,7 @@ static PyObject *symnmf_wrapper(PyObject *self, PyObject *args);
  * ============================================================================
  */
 
-// Method definition object for this extension, mapping python method names to C functions
+/* Method definition object for this extension, mapping python method names to C functions */
 static PyMethodDef symnmf_methods[] = {
     {
         "sym",
@@ -50,8 +50,8 @@ static PyMethodDef symnmf_methods[] = {
  */
 static struct PyModuleDef symnmf_module = {
     PyModuleDef_HEAD_INIT,
-    "mysymnmf",                                  // Module name
-    "Python C API for symmetric NMF algorithm.", // Module documentation
+    "mysymnmf",
+    "Python C API for symmetric NMF algorithm.",
     -1,
     symnmf_methods};
 
@@ -70,29 +70,33 @@ PyMODINIT_FUNC PyInit_mysymnmf(void) {
  * ============================================================================
  */
 
-// Helper function to convert a Python list of lists to a C double**
+/* Helper function to convert a Python list of lists to a C double** */
 double **matrix_py_to_c(PyObject *py_matrix, int n, int m) {
-    // 1. Get dimensions n (rows) and d (cols).
-    // 2. Allocate memory for a C double** matrix.
-    // 3. Iterate through the Python list, converting each element to double and populating the C matrix.
-    // 4. Return the C matrix. Return NULL on failure.
     Py_ssize_t i, j;
-    double **c_matrix = (double **)malloc((n) * sizeof(double *));
+    PyObject *row, *item;
+    double **c_matrix;
+
+    c_matrix = (double **)malloc((n) * sizeof(double *));
     if (!c_matrix)
         return NULL;
+
     for (i = 0; i < n; i++) {
-        PyObject *row = PyList_GetItem(py_matrix, i);
+        /* Read rows */
+        row = PyList_GetItem(py_matrix, i);
         if (!PyList_Check(row) || PyList_Size(row) != m) {
             free_matrix(c_matrix, i);
             return NULL;
         }
+
         c_matrix[i] = (double *)malloc((m) * sizeof(double));
         if (!c_matrix[i]) {
             free_matrix(c_matrix, i);
             return NULL;
         }
+
         for (j = 0; j < m; j++) {
-            PyObject *item = PyList_GetItem(row, j);
+            /* Read columns */
+            item = PyList_GetItem(row, j);
             c_matrix[i][j] = PyFloat_AsDouble(item);
             if (PyErr_Occurred()) {
                 free_matrix(c_matrix, i);
@@ -100,41 +104,47 @@ double **matrix_py_to_c(PyObject *py_matrix, int n, int m) {
             }
         }
     }
+
     return c_matrix;
 }
 
-// Helper function to convert a C double** to a Python list of lists
+/* Helper function to convert a C double** to a Python list of lists */
 PyObject *matrix_c_to_py(double **c_matrix, int n, int m) {
-    // 1. Create a new Python list (PyList_New).
-    // 2. Iterate through the C matrix. For each row, create a new Python list.
-    // 3. For each element in the row, create a Python float (PyFloat_FromDouble) and add it to the row list.
-    // 4. Add the row list to the main list.
-    // 5. Return the main Python list.
+    PyObject *py_matrix, *row_list, *num;
+    int i, j;
+
     if (!c_matrix)
         return NULL;
 
-    PyObject *py_matrix = PyList_New(n);
+    py_matrix = PyList_New(n);
     if (!py_matrix)
         return NULL;
-    for (int i = 0; i < n; i++) {
-        PyObject *row_list = PyList_New(m);
+
+    for (i = 0; i < n; i++) {
+        /* Read rows */
+        row_list = PyList_New(m);
         if (!row_list) {
             Py_DECREF(py_matrix);
             free_matrix(c_matrix, n);
             return NULL;
         }
-        for (int j = 0; j < m; j++) {
-            PyObject *num = PyFloat_FromDouble(c_matrix[i][j]);
+
+        for (j = 0; j < m; j++) {
+            /* Read columns */
+            num = PyFloat_FromDouble(c_matrix[i][j]);
             if (!num) {
                 Py_DECREF(row_list);
                 Py_DECREF(py_matrix);
                 free_matrix(c_matrix, n);
                 return NULL;
             }
+
             PyList_SET_ITEM(row_list, j, num);
         }
+
         PyList_SET_ITEM(py_matrix, i, row_list);
     }
+
     return py_matrix;
 }
 
@@ -144,105 +154,157 @@ PyObject *matrix_c_to_py(double **c_matrix, int n, int m) {
  * ============================================================================
  */
 
+/* Sym function calls */
 double **_sym_wrapper(PyObject *points_py, int n, int d) {
     double **points_c, **sym_c;
+
+    /* Translate point matrix to C */
     points_c = matrix_py_to_c(points_py, n, d);
     Py_DECREF(points_py);
     if (!points_c)
         return NULL;
+
+    /* Calculate sym matrix */
     sym_c = calc_sym(points_c, n, d);
     free_matrix(points_c, n);
+
     return sym_c;
 }
 
+/* Wrapper for sym function */
 static PyObject *sym_wrapper(PyObject *self, PyObject *args) {
     PyObject *points_py, *sym_py;
     int n, d;
     double **sym_c;
+
     if (!PyArg_ParseTuple(args, "Oii", &points_py, &n, &d))
         return NULL;
+
+    /* Calculate sym matrix*/
     sym_c = _sym_wrapper(points_py, n, d);
     if (!sym_c)
         return NULL;
+
+    /* Translate matrix to Python*/
     sym_py = matrix_c_to_py(sym_c, n, d);
     free_matrix(sym_c, n);
+
     return sym_py;
 }
 
+/* Dgg function calls */
 double **_dgg_wrapper(PyObject *points_py, int n, int d) {
     double **sym_c, **dgg_c;
+
+    /* Calculate sym matrix */
     sym_c = _sym_wrapper(points_py, n, d);
     if (!sym_c)
         return NULL;
+
+    /* Calculate ddg matrix */
     dgg_c = calc_ddg(sym_c, n);
     free_matrix(sym_c, n);
+
     return dgg_c;
 }
 
+/* Wrapper for ddg function */
 static PyObject *ddg_wrapper(PyObject *self, PyObject *args) {
     PyObject *points_py, *dgg_py;
     int n, d;
     double **dgg_c;
+
     if (!PyArg_ParseTuple(args, "Oii", &points_py, &n, &d))
         return NULL;
+
+    /* Calculate dgg matrix */
     dgg_c = _dgg_wrapper(points_py, n, d);
     if (!dgg_c)
         return NULL;
+
+    /* Translate matrix to Python */
     dgg_py = matrix_c_to_py(dgg_c, n, d);
     free_matrix(dgg_c, n);
+
     return dgg_py;
 }
 
+/* Norm function calls */
 double **_norm_wrapper(PyObject *points_py, int n, int d) {
     double **sym_c, **dgg_c, **norm_c;
+
+    /* Calculate sym matrix */
     sym_c = _sym_wrapper(points_py, n, d);
     if (!sym_c)
         return NULL;
+
+    /* Calculate ddg matrix */
     dgg_c = calc_ddg(sym_c, n);
     if (!dgg_c) {
         free_matrix(sym_c, n);
         return NULL;
     }
+
+    /* Calculate norm matrix */
     norm_c = calc_norm(sym_c, dgg_c, n);
     free_matrix(sym_c, n);
     free(dgg_c, n);
+
     return norm_c;
 }
 
+/* Wrapper for norm function */
 static PyObject *norm_wrapper(PyObject *self, PyObject *args) {
     PyObject *points_py, *norm_py;
     int n, d;
     double **norm_c;
+
     if (!PyArg_ParseTuple(args, "Oii", &points_py, &n, &d))
         return NULL;
+
+    /* Calculate norm matrix */
     norm_c = _norm_wrapper(points_py, n, d);
     if (!norm_c)
         return NULL;
+
+    /* Translate matrix to Python*/
     norm_py = matrix_c_to_py(norm_c, n, d);
     free_matrix(norm_c, n);
+
     return norm_py;
 }
 
+/* Wrapper for symnmf function */
 static PyObject *symnmf_wrapper(PyObject *self, PyObject *args) {
     PyObject *W_py, *H_init_py, *H_py;
     double **W_c, **H_init_c, **H_c;
     int n, k;
+
     if (!PyArg_ParseTuple(args, "OOii", &W_py, &H_init_py, &n, &k))
         return NULL;
+
+    /* Translate W matrix to C */
     W_c = matrix_py_to_c(W_py, n, n);
     Py_DECREF(W_py);
     if (!W_c)
         return NULL;
+
+    /* Translate initial H matrix to C */
     H_init_c = matrix_py_to_c(H_init_py, n, k);
     Py_DECREF(H_init_py);
     if (!H_init_c) {
         free_matrix(W_c, n);
         return NULL;
     }
+
+    /* Calculate H matrix */
     H_c = calc_symnmf(W_c, H_init_c, n, k);
     free_matrix(W_c, n);
     free_matrix(H_init_c, n);
+
+    /* Translate H matrix to Python*/
     H_py = matrix_c_to_py(H_c, n, k);
     free_matrix(H_c, n);
+
     return H_py;
 }
